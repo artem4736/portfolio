071de7dbceb046fc8b3e8ec1d50e05b1188860fd
artem4736 <saa75752@gmail.com>
2025-10-06 15:20:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,20 @@ window.addEventListener('load', () => {
   });
 });
 // Темний режим
- const darkModeBtn = document.getElementById('darkModeBtn');
-darkModeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
+ const btn = document.getElementById('darkModeBtn');
+  const html = document.documentElement;
+
+  if (localStorage.getItem('theme') === 'dark') {
+    html.classList.add('dark');
+  }
+
+  btn.addEventListener('click', () => {
+    html.classList.toggle('dark');
+    if (html.classList.contains('dark')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
+  });
+
 
