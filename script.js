b69f2d5eea09647ed5b4a1213e72d2f873f51809
artem4736 <saa75752@gmail.com>
2025-10-22 10:08:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,8 +31,7 @@ if (darkModeBtn) {
   });
 }
 const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
-
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
